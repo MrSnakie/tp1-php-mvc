27184356e5bbb2edf11e4de35a01fae671e13a16
MrSnakie <petitloulou96@free.fr>
2017-03-31 15:09:48 +0200

    Change list URL
    For users & services

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -8,11 +8,11 @@
 <div class="row">
 	<div class="col-md-6">
 		<p><a href="admin.php?p=users.add" class="btn btn-info btn-lg size" role="button">Ajouter un utilisateur</a></p>
-		<p><a href="admin.php?p=users.edit" class="btn btn-info btn-lg size" role="button">Liste des utilisateurs</a></p>
+		<p><a href="admin.php?p=users.list" class="btn btn-info btn-lg size" role="button">Liste des utilisateurs</a></p>
 	</div>
 
 	<div class="col-md-6">
 		<p><a href="admin.php?p=services.add" class="btn btn-info btn-lg size" role="button">Ajouter un service</a></p>
-		<p><a href="admin.php?p=services.edit" class="btn btn-info btn-lg size" role="button">Liste des services</a></p>
+		<p><a href="admin.php?p=services.list" class="btn btn-info btn-lg size" role="button">Liste des services</a></p>
 	</div>
 </div>
